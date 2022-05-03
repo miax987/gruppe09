fccb2e73c041481639c29fd8c2856e051bdbc00f
hai dong <hai_dong1@yahoo.com>
2022-05-04 00:20:22 +0200

    update table models

--- hubbackend/app/models/index.js
+++ hubbackend/app/models/index.js
@@ -21,5 +21,17 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+db.lessonplan= require("./lessonplan.js")(sequelize, Sequelize);
+db.announcements= require("./announcements.js")(sequelize, Sequelize);
+db.appointment= require("./appointment.js")(sequelize, Sequelize);
+db.assignment= require("./assignment.js")(sequelize, Sequelize);
+db.course= require("./course.js")(sequelize, Sequelize);
+db.grade= require("./grade.js")(sequelize, Sequelize);
+db.lesson= require("./lesson.js")(sequelize, Sequelize);
+db.location= require("./location.js")(sequelize, Sequelize);
+db.studentCourses= require("./studentCourses.js")(sequelize, Sequelize);
+db.student= require("./student.js")(sequelize, Sequelize);
+db.studyclass= require("./studyclass.js")(sequelize, Sequelize);
+db.studyprogramme= require("./studyprogramme.js")(sequelize, Sequelize);
 
 module.exports = db;
